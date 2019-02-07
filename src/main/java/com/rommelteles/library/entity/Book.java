fd99a6b369a0be902c5283c4a810b5d724fddaed
Rommel Teles <rommelteles@MacBook-de-Rommel.local>
2019-02-07 19:08:00 +0000

    Entity

---
+++ src/main/java/com/rommelteles/library/entity/Book.java
@@ -0,0 +1,22 @@
+package com.rommelteles.library.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="TB_BOOK")
+public class Book  implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8935587443390313693L;
+	
+	
+	
+	
+	
+
+}
